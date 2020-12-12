a90536b3091e0d1473f45d58dbae5b32ce66ffef
Sir Quarian <sir.quarian@theknightsofunity.com>
2020-12-12 21:41:10 +0100

    Beat

---
+++ Assets/Scripts/Beat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour
+{
+    [SerializeField] private float bpm = 60;
+    [SerializeField] private float acceptableOffset = 0.5f;
+
+    public static Action OnBeat;
+
+    private static double acceptable;
+    private static double time = 0;
+    private static double beatInterval;
+
+    private void Start()
+    {
+        beatInterval = 60 / bpm;
+        acceptable = acceptableOffset;
+        OnBeat += () => Debug.Log("Beap!");
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        if (time >= beatInterval)
+        {
+            OnBeat?.Invoke();
+            time -= beatInterval;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+            Debug.Log(TryBeat());
+    }
+
+    public static bool TryBeat()
+    {
+        double offset = Mathf.Min((float)time,(float) (beatInterval - time));
+        return offset < acceptable;
+    }
+    
+}
---
+++ Assets/Scripts/BeatUIIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BeatUIIndicator : MonoBehaviour
+{
+    [SerializeField] private float duration = 0.3f;
+    
+    private Image image;
+
+    private void OnEnable()
+    {
+        Beat.OnBeat += OnBeat;
+    }
+
+    private void OnDisable()
+    {
+        Beat.OnBeat -= OnBeat;
+    }
+
+    private void OnBeat()
+    {
+        if (image is null)
+            image = GetComponent<Image>();
+
+        image.DOFade(1, duration).From();
+    }
+}
