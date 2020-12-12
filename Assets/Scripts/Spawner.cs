6610dd9aa3cd25f1b99f5e79b7abaa4af8e2534e
Toster <tostermocy@gmail.com>
2020-12-13 00:00:08 +0100

    something

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private int index = 0;
+    public GameObject[] EnemyPrefab;
+    public void Spawn()
+    {
+        index = Random.Range (0, EnemyPrefab.Length);
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+   private IEnumerator coroutine;
+   public Spawner[] spawnersArray;
+   public float spawnCooldown = 2.0f;
+   private int index = 0;
+       void Start()
+       {
+
+           coroutine = CallSpawn(spawnCooldown);
+           StartCoroutine(coroutine);
+       }
+   
+       private IEnumerator CallSpawn(float waitTime)
+       {
+           while (true)
+           {
+               index = Random.Range (0, spawnersArray.Length);
+               spawnersArray[index].Spawn();
+               yield return new WaitForSeconds(waitTime);
+           }
+       }
+}
