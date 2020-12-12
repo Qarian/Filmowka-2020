ef32be9d0310f3fd00d609d00b0eb15be79b179c
Sir Quarian <sir.quarian@theknightsofunity.com>
2020-12-12 19:44:44 +0100

    Simple shooting

---
+++ Assets/Scripts/Destroyable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    public class Destroyable : MonoBehaviour
+    {
+        [SerializeField] private float health = 100;
+
+        public void DealDamage(float damage)
+        {
+            health -= damage;
+            if (health <= 0)
+                Destroy();
+        }
+
+        private void Destroy()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
